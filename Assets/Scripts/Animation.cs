73e0af80a7ed6a415e7e74098935d4e2f8b76d2a
Jaeseong Lim <dlawotjd1020@gmail.com>
2019-10-23 17:24:28 +0900

    + , 취소 , 확인 버튼 구현

---
+++ Assets/Scripts/Animation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    Animator animator;
+    public ButtonManager buttonManager;
+    public readonly int hashPopup = Animator.StringToHash("isPopup");
+    private void Start()
+    {    
+        animator = GetComponent<Animator>();
+        animator.SetBool(hashPopup, false);
+        buttonManager.buttonclick = (isOn) =>
+        {
+            if (isOn == true)
+            {
+                Popup();
+            }
+            else
+            {
+                PopupClose();
+            }
+        };
+    }
+    public void Popup()
+    {
+        animator.SetBool(hashPopup, true);
+    }
+    public void PopupClose()
+    {
+        animator.SetBool(hashPopup, false);
+    }
+}
---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ButtonManager : MonoBehaviour
+{
+    public Action<bool> buttonclick;
+    public static bool isOn;
+    private void Start()
+    {
+        isOn = false;
+    }
+    public void OpnePopup()
+    {
+        isOn = true;
+        buttonclick(isOn);
+    }
+    public void ClosePopup()
+    {
+        isOn = false;
+        buttonclick(isOn);
+    }
+    public void Save()
+    {
+
+    }
+}
